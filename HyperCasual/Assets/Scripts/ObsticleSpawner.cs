d4a0e3ce1d1688a58048308285b69541efd18955
minigaindev <talhaozturkx1@gmail.com>
2021-12-29 23:58:58 +0300

    Added Obsticles and Code Review

---
+++ HyperCasual/Assets/Scripts/ObsticleSpawner.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticleSpawner : MonoBehaviour
+{
+    public GameObject[] Obsticles;
+    public Transform[] ObsticleSpawnPoints;
+
+    private void Start()
+    {
+        
+    }
+}
